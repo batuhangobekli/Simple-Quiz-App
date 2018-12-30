018db4b46aa49a8b3a8346bdf728e48e87e2666f
batuhangobekli <btubatuhan@gmail.com>
2018-12-30 23:40:52 +0300

    Landing page created

--- quiz_app/lib/Pages/landing_page.dart
+++ quiz_app/lib/Pages/landing_page.dart
@@ -5,6 +5,16 @@ class LandingPage extends StatelessWidget{
    Widget build(BuildContext context){
      return new Material(
        color: Colors.greenAccent,
-     ); 
+       child: new InkWell(
+         onTap: () => print("We tapped the page!"),
+         child: new Column(
+           mainAxisAlignment: MainAxisAlignment.center,
+           children: <Widget>[
+             new Text("Quiz It",style: new TextStyle(color: Colors.white,fontSize: 50.0,fontWeight:FontWeight.bold),),
+             new Text("Tap To start",style: new TextStyle(color: Colors.white,fontSize: 20.0,fontWeight:FontWeight.bold),)
+           ],
+         ),
+       ),
+     );
    }
 }
